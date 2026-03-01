66a4c9970b1b9ba101bc221863a80c186ffdd080
Aarsh31248 <starfire9599@gmail.com>
2026-03-01 20:34:48 +0530

    Time: 1 ms (48.99%), Space: 58.8 MB (16.59%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (arr) {
+    let x = 0;
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > arr[x]) {
+            x = x + 1;
+            arr[x] = arr[i]
+        }
+    }
+    return x + 1;
+};
